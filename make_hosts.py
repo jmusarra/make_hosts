9b295567b386e029986b92d56a2db29b97b9835e
jmusarra <github@mightymu.net>
2023-10-20 15:46:24 -0700

    Column justification attempt

--- make_hosts.py
+++ make_hosts.py
@@ -12,14 +12,30 @@ from pathlib import Path
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 
+
+hosts_file = 'C:\\Windows\\System32\\drivers\\etc\\hosts'
 # Open a filepicker dialog:
 Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
 ip_doc_source = Path(askopenfilename(title = "IP Document location", filetypes=[("Excel files", ".xlsx .xls")]))
+#escape the quotes in the filename:
+ip_doc_source = f'{ip_doc_source}"'
 if os.path.isfile(ip_doc_source):
-    print('yup we see it')
+    print('yes, that seems like a valid file')
     print(f'Using source file: {str(ip_doc_source)}')
 
-# make a local copy of the document so we're not trying to work with an active document
+# TODO make a backup of the existing hosts file:
+hosts_file_backup = f'{hosts_file}_backup-{datetime.now().strftime("%Y%m%d")}'
+backup_command = f'powershell.exe copy {hosts_file} {hosts_file_backup}'
+print(backup_command)
+windows_result = os.system(backup_command)
+print(f'Powershell says: {windows_result}')
+if windows_result == 1:
+	print('Windows copy command exited abmormally, possibly due to a PermissionDenied error. Try running as administrator')
+	time.sleep(3)
+if os.path.isfile(hosts_file_backup):
+	print(f'Existing hosts file backed up at {hosts_file_backup}')
+
+# make a local copy of the document so we're not trying to work with an active document:
 ip_doc_temp = str(Path.home()) + '\\ip_doc_temp.xlsx'
 # just in case there's an old temp file hanging out, remove it:
 if os.path.isfile(ip_doc_temp):
@@ -31,7 +47,7 @@ elif os.path.isdir(ip_doc_temp):
 
 command_escaped = f'powershell.exe copy \\"{ip_doc_source}\\" "{ip_doc_temp}"'
 try:
-    #powershell.exe copy \"C:\Users\jmusarra\ip_doc.xlsx\" "C:\Users\jmusarra\ip_doc_temp.xlsx"
+    #powershell.exe copy \"C:\Users\jmusarra\ip_doc.xlsx\" "C:\Users\jmusarra\ip_doc_temp.xlsx" - for reference
     os.system(command_escaped)
     if os.path.isfile(ip_doc_temp):
         print(command_escaped)
@@ -41,10 +57,9 @@ try:
 except PermissionError:
 	print("oh goddamnit")
 if os.path.isfile(ip_doc_temp):
-    print('ok the copy worked...')
     print(f'IP doc temp location: {ip_doc_temp}')
 else:
-	print('ok the copy failed...')
+	print('hm the copy failed...')
 
 # windows boilerplate:
 faff = '''
@@ -75,7 +90,7 @@ faff = '''
 '''
 
 # make a local copy of the document :
-destination_file = "C:\\Windows\\System32\\drivers\\etc\\hosts"
+#destination_file = "C:\\Windows\\System32\\drivers\\etc\\hosts"
 if os.path.isfile(ip_doc_temp):
     with pandas.ExcelFile(ip_doc_temp) as file:
         print(f'Document found: {ip_doc_temp}.')
@@ -84,23 +99,21 @@ if os.path.isfile(ip_doc_temp):
         print(sheets)
         # make Pandas dataframes from selected worksheets:
         print('Reticulating splines...')
-        # TODO: check if the worksheets exist first
         available_sheets = []
-        # hm I could do this as a for... available_sheets[0]...        
         if 'ARCH_LTG IP' in sheets:
-        	print('arch_ltg ip')
+        	print('found ARCH_LTG IP')
         	arch_ltg_ip = pandas.read_excel(ip_doc_temp, sheet_name = "ARCH_LTG IP", header = 4, index_col = None, usecols = ['DEVICE ID', 'IP ADDRESS'])
         	available_sheets.append(arch_ltg_ip)
         else:
         	print('nope')
         if 'PROD_LTG IP' in sheets:
-        	print('prod_ltg ip')
+        	print('found PROD_LTG IP')
         	prod_ltg_ip = pandas.read_excel(ip_doc_temp, sheet_name = "PROD_LTG IP", header = 4, index_col = None, usecols = ['DEVICE ID', 'IP ADDRESS'])
         	available_sheets.append(prod_ltg_ip)
         else:
         	print('nope')
         if 'ARCH_CTRL IP' in sheets:
-        	print('arch_ctrl ip')
+        	print('found ARCH_CTRL IP')
         	arch_ctrl_ip = pandas.read_excel(ip_doc_temp, sheet_name = "ARCH_CTRL IP", header = 4, index_col = None, usecols = ['DEVICE ID', 'IP ADDRESS'])
         	available_sheets.append(arch_ctrl_ip)
         else:
@@ -127,15 +140,16 @@ print('Done')
 num_devices = (f'# Number of devices: {merged_frames.shape[0]}\n\n')
 
 with pandas.option_context('display.max_rows', None):
-	hosts = merged_frames.to_string(index = False, header = False)
+#	hosts = merged_frames.apply(left_justify)
+	hosts = merged_frames.to_string(index = False, header = False, formatters = {'IP ADDRESS': (lambda x: '{:<9}'.format(x))}, justify='left')
 
 generated_date = f'# Date generated: {datetime.now().strftime("%Y-%m-%d %H:%M")}\n'
 
 print('Writing file....')
-with open(destination_file, 'w') as f:
+with open(hosts_file, 'w') as f:
 	f.write(faff + generated_date + num_devices + hosts)
 
-print(f"Hosts file generation complete. Written to {destination_file}.")
+print(f"Hosts file generation complete. Written to {hosts_file}.")
 print("Removing temporary files")
 
 # clean up temporary file 😬
