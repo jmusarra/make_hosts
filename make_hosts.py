7a2174c3cad90967a801f2c549b44dee80603450
jmusarra <github@mightymu.net>
2023-10-23 12:34:58 -0700

    linting

--- make_hosts.py
+++ make_hosts.py
@@ -1,25 +1,31 @@
 '''
-Takes IP addresses and device IDs from a spreadsheet and exports that info into a Windows-readable hosts file
+Takes IP addresses and device IDs from a spreadsheet and exports that info into
+ a Windows-readable hosts file.
 Hosts file info: https://en.wikipedia.org/wiki/Hosts_(file)
 Looks for worksheets named "ARCH_LTG IP", "PROD_LTG_IP", and "ARCH_CTRL_IP"
-Only rows with both DEVICE ID and IP ADDRESS will be written to hosts file. If either of those columns are 
- mising data, that row will be ignored.
+Only rows with both DEVICE ID and IP ADDRESS will be written to hosts file. If 
+either of those columns are mising data, that row will be ignored.
 '''
 # pandas is a python data analysis and manipulation library - https://pandas.pydata.org/
-import pandas, sys, os, time
+# TODO lint
+import sys
+import os
+import time
+
 from datetime import datetime
 from pathlib import Path
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
+import pandas
 
-hosts_file = 'C:\\Windows\\System32\\drivers\\etc\\hosts'
+
+HOSTS_FILE = 'C:\\Windows\\System32\\drivers\\etc\\hosts'
+hosts_file_backup_location = f'{Path.home()}\\hosts-backup'
 # Open a filepicker dialog:
 Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
 ip_doc_source = Path(askopenfilename(title = "IP Document location", filetypes=[("Excel files", ".xlsx .xls")]))
-print(str(type(ip_doc_source)))
-print(ip_doc_source)
 if ip_doc_source == Path('.'):
-	sys.exit('No source document slected. Exiting')
+    sys.exit('No source document slected. Exiting')
 #escape the quotes in the filename:
 ip_doc_source = f'{ip_doc_source}"'
 if os.path.isfile(ip_doc_source):
@@ -27,64 +33,59 @@ if os.path.isfile(ip_doc_source):
     print(f'Using source file: {str(ip_doc_source)}')
 
 # TODO make a backup of the existing hosts file:
-hosts_file_backup = f'{hosts_file}_backup-{datetime.now().strftime("%Y%m%d")}'
-backup_command = f'powershell.exe copy {hosts_file} {hosts_file_backup}'
+hosts_file_backup = f'{HOSTS_FILE}_backup-{datetime.now().strftime("%Y%m%d")}'
+backup_command = f'powershell.exe copy {HOSTS_FILE} {hosts_file_backup}'
 print(backup_command)
 windows_result = os.system(backup_command)
 print(f'Powershell says: {windows_result}')
 if windows_result == 1:
-	# TODO Make this ask for an ok before proceeding without backup
-	question = input(''' This script is running without administrator privileges. Therefore it cannot create a new file
-	inside the Windows\\System32\\drivers\\etc directory. You can proceed by overwriting the existing hosts file, or
-	create the backup somewhere we have write access to.
-	To skip making a backup, type 's'
-	To make a backup in your home directory, type 'h'
-	To overwrite any existing hosts file, type 'o'
-	To make a backup in-place, type 'q' to quit, and re-run as Administrator.
-	s, h, o, q? \n''')
-	if question == 's':
-		print('S!')
-	elif question == 'h':
-		print('H!')
-	elif question == 'o':
-		print('You have chosen to overwrite! WE ARE ALL DOOOOMED!!')
-	else:
-	    sys.exit('Exiting. To run as administrator, right-click and select "Run as Administrator"') 
-
-	print('Windows copy command exited abmormally, possibly due to a PermissionDenied error. Try running as administrator')
-	sys.exit("\nWe are not going to overwrite a file without user's explicit consent. Exiting.")
-	time.sleep(3)
+    question = input(f''' This script is running without administrator privileges.
+ Therefore it cannot create a new file inside the Windows\\System32\\drivers\\etc 
+directory. You can proceed by overwriting the existing hosts file, or create the 
+backup somewhere we have write access to.
+    To overwrite any existing hosts file, type 'o'
+    To make a backup in {hosts_file_backup_location}, type 'h'
+    To make a backup in-place, type 'q' to quit, and re-run as Administrator.
+    h, o, q? \n''')
+    if question == 'h':
+        print('H!')
+    elif question == 'o':
+        print('You have chosen to overwrite! WE ARE ALL DOOOOMED!!')
+    else:
+        sys.exit('Exiting. To run as administrator, right-click and select "Run as Administrator"')
+        #sys.exit("\nWe are not going to overwrite a file without user's explicit consent. Exiting.")
+    time.sleep(3)
 if os.path.isfile(hosts_file_backup):
-	print(f'Existing hosts file backed up at {hosts_file_backup}')
+    print(f'Existing hosts file backed up at {hosts_file_backup}')
 
 # make a local copy of the document so we're not trying to work with an active document:
-ip_doc_temp = str(Path.home()) + '\\ip_doc_temp.xlsx'
+IP_DOC_TEMP = str(Path.home()) + '\\ip_doc_temp.xlsx'
 # just in case there's an old temp file hanging out, remove it:
-if os.path.isfile(ip_doc_temp):
-	print('old temp file found, removing')
-	os.remove(ip_doc_temp)
-elif os.path.isdir(ip_doc_temp):
+if os.path.isfile(IP_DOC_TEMP):
+    print('old temp file found, removing')
+    os.remove(IP_DOC_TEMP)
+elif os.path.isdir(IP_DOC_TEMP):
     print("old temp file found, except it's a directory. Removing.")
-    os.rmdir(ip_doc_temp)
+    os.rmdir(IP_DOC_TEMP)
 
-command_escaped = f'powershell.exe copy \\"{ip_doc_source}\\" "{ip_doc_temp}"'
+command_escaped = f'powershell.exe copy \\"{ip_doc_source}\\" "{IP_DOC_TEMP}"'
 try:
     #powershell.exe copy \"C:\Users\jmusarra\ip_doc.xlsx\" "C:\Users\jmusarra\ip_doc_temp.xlsx" - for reference
     os.system(command_escaped)
-    if os.path.isfile(ip_doc_temp):
+    if os.path.isfile(IP_DOC_TEMP):
         print(command_escaped)
         print('Great, copy sucesful. Moving on.')
     else:
         print('Copy failed somehow wtf')
 except PermissionError:
-	print("oh goddamnit")
-if os.path.isfile(ip_doc_temp):
-    print(f'IP doc temp location: {ip_doc_temp}')
+    print("oh goddamnit")
+if os.path.isfile(IP_DOC_TEMP):
+    print(f'IP doc temp location: {IP_DOC_TEMP}')
 else:
-	print('hm the copy failed...')
+    print('hm the copy failed...')
 
 # windows boilerplate:
-faff = '''
+FAFF = '''
 # Copyright (c) 1993-2009 Microsoft Corp.
 #
 # This is a sample HOSTS file used by Microsoft TCP/IP for Windows.
@@ -113,9 +114,9 @@ faff = '''
 
 # make a local copy of the document :
 #destination_file = "C:\\Windows\\System32\\drivers\\etc\\hosts"
-if os.path.isfile(ip_doc_temp):
-    with pandas.ExcelFile(ip_doc_temp) as file:
-        print(f'Document found: {ip_doc_temp}.')
+if os.path.isfile(IP_DOC_TEMP):
+    with pandas.ExcelFile(IP_DOC_TEMP) as file:
+        print(f'Document found: {IP_DOC_TEMP}.')
         # check if our target sheets (ARCH_LTG IP, PROD_LTG IP, ARCH_CTRL IP) exist:
         sheets = file.sheet_names
         print(sheets)
@@ -123,28 +124,28 @@ if os.path.isfile(ip_doc_temp):
         print('Reticulating splines...')
         available_sheets = []
         if 'ARCH_LTG IP' in sheets:
-        	print('found ARCH_LTG IP')
-        	arch_ltg_ip = pandas.read_excel(ip_doc_temp, sheet_name = "ARCH_LTG IP", header = 4, index_col = None, usecols = ['DEVICE ID', 'IP ADDRESS'])
-        	available_sheets.append(arch_ltg_ip)
+            print('found ARCH_LTG IP')
+            arch_ltg_ip = pandas.read_excel(IP_DOC_TEMP, sheet_name = "ARCH_LTG IP", header = 4, index_col = None, usecols = ['DEVICE ID', 'IP ADDRESS'])
+            available_sheets.append(arch_ltg_ip)
         else:
-        	print('nope')
+            print('nope')
         if 'PROD_LTG IP' in sheets:
-        	print('found PROD_LTG IP')
-        	prod_ltg_ip = pandas.read_excel(ip_doc_temp, sheet_name = "PROD_LTG IP", header = 4, index_col = None, usecols = ['DEVICE ID', 'IP ADDRESS'])
-        	available_sheets.append(prod_ltg_ip)
+            print('found PROD_LTG IP')
+            prod_ltg_ip = pandas.read_excel(IP_DOC_TEMP, sheet_name = "PROD_LTG IP", header = 4, index_col = None, usecols = ['DEVICE ID', 'IP ADDRESS'])
+            available_sheets.append(prod_ltg_ip)
         else:
-        	print('nope')
+            print('nope')
         if 'ARCH_CTRL IP' in sheets:
-        	print('found ARCH_CTRL IP')
-        	arch_ctrl_ip = pandas.read_excel(ip_doc_temp, sheet_name = "ARCH_CTRL IP", header = 4, index_col = None, usecols = ['DEVICE ID', 'IP ADDRESS'])
-        	available_sheets.append(arch_ctrl_ip)
+            print('found ARCH_CTRL IP')
+            arch_ctrl_ip = pandas.read_excel(IP_DOC_TEMP, sheet_name = "ARCH_CTRL IP", header = 4, index_col = None, usecols = ['DEVICE ID', 'IP ADDRESS'])
+            available_sheets.append(arch_ctrl_ip)
         else:
-        	print('nope')
+            print('nope')
         # merge each of the individual dataframes into a complete dataframe:
         merged_frames = pandas.concat(available_sheets)
         print('Done.')
 else:
-	sys.exit("Could not find IP document. Exiting.")
+    sys.exit("Could not find IP document. Exiting.")
 
 # rearrange columns as needed for hosts file order:
 print("Reordering columns...")
@@ -159,21 +160,21 @@ merged_frames['IP ADDRESS'] = merged_frames['IP ADDRESS'].str.replace(' ', '')
 print('Dropping incomplete rows...')
 merged_frames.dropna(how = 'any', inplace = True)
 print('Done')
-num_devices = (f'# Number of devices: {merged_frames.shape[0]}\n\n')
+num_devices = f'# Number of devices: {merged_frames.shape[0]}\n\n'
 
 with pandas.option_context('display.max_rows', None):
 #	hosts = merged_frames.apply(left_justify)
-	hosts = merged_frames.to_string(index = False, header = False, formatters = {'IP ADDRESS': (lambda x: '{:<9}'.format(x))}, justify='left')
+    hosts = merged_frames.to_string(index = False, header = False, formatters = {'IP ADDRESS': (lambda x: '{:<9}'.format(x))}, justify='left')
 
 generated_date = f'# Date generated: {datetime.now().strftime("%Y-%m-%d %H:%M")}\n'
 
 print('Writing file....')
-with open(hosts_file, 'w') as f:
-	f.write(faff + generated_date + num_devices + hosts)
+with open(HOSTS_FILE, 'w', encoding='cp1252') as f:
+    f.write(FAFF + generated_date + num_devices + hosts)
 
-print(f"Hosts file generation complete. Written to {hosts_file}.")
+print(f"Hosts file generation complete. Written to {HOSTS_FILE}.")
 print("Removing temporary files")
 
 # clean up temporary file 😬
-os.remove(ip_doc_temp)
+os.remove(IP_DOC_TEMP)
 time.sleep(10)
